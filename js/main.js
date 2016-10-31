e7443e649788456544f5b7a96d99f034d70459a2
Dokiz_228@mail.ru <deoptgqqr12>
2016-10-31 16:20:27 +0300

    start

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var Person = function(config){
+    this.name = config.name;
+    this.age = config.age;
+    this.job = config.job;
+};
+
+Person.prototype.walk = function(){
+  return this.name + ' is walking';
+};
+
+var nick = new Person({name: 'Nick', age:'24',job:'Front-end-developer'});
+
+var Person = Backbone.Model.extend({// на жаргоне это класс
+   defaults: {
+       name: 'Dima',
+       age: 23,
+       job: 'web-developer'
+ },
+
+   walk:function(){
+        return this.get('name')+ ' is walking'
+    }
+
+});
+
+//var person = new Person;
