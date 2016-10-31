1909d8585d299ff16be2d6308477a7526f21e362
Dokiz_228@mail.ru <deoptgqqr12>
2016-10-31 17:32:21 +0300

    start

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var Person = function(config){
+/*var Person = function(config){
     this.name = config.name;
     this.age = config.age;
     this.job = config.job;
@@ -9,13 +9,24 @@ Person.prototype.walk = function(){
 };
 
 var nick = new Person({name: 'Nick', age:'24',job:'Front-end-developer'});
-
+*/
 var Person = Backbone.Model.extend({// на жаргоне это класс
    defaults: {
        name: 'Dima',
        age: 23,
        job: 'web-developer'
  },
+ 
+ 
+ validate:function(attrs ){
+	 console.log(attrs);
+	 if (attrs.age <= (0)){
+		 return 'Возраст должен быть положительным';
+		 
+	 }
+	 
+ },
+ 
 
    walk:function(){
         return this.get('name')+ ' is walking'
